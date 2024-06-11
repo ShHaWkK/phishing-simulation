2ecc206bba9db2e60ee9d214b8bdaa62c73e8709
ShHaWkK <alexandreuzan9@gmail.com>
2024-06-11 11:01:41 +0200

    ADD Settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'phishing_simulation_db',
+        'USER': 'user',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
